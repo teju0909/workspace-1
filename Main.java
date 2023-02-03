1fa22a7f7fa0f074a69e15ce371b89b118a2c433
Nikita <kamblenikita593@gmail.com>
2023-02-03 13:19:54 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void call(Integer a){
+        //int a=a;
+        System.out.println("a:"+a);
+       }
+  
+    public static void main(String[] arg){
+      
+        call(null);
+        
+       }
+
+    }
+
+
+   
